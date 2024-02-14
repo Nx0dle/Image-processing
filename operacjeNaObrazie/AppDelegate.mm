5f46b46755517bde222b1d18264ffbc40c3593d5
Przemek Paciorek <109921311+Nx0dle@users.noreply.github.com>
2024-02-14 13:44:07 +0100

    box blur algorithm ( to check!! ) + code refactor

--- operacjeNaObrazie/AppDelegate.mm
+++ operacjeNaObrazie/AppDelegate.mm
@@ -11,6 +11,7 @@
 #include <cstring>
 using namespace std;
 
+#define RGBA(i) (i).r, (i).g, (i).b, (i).a
 @interface AppDelegate ()
 
 @property (strong) IBOutlet NSWindow *window;
@@ -18,12 +19,62 @@ using namespace std;
 
 @implementation AppDelegate
 
-typedef struct rgba {
+typedef struct {
     uint8_t r;
     uint8_t g;
     uint8_t b;
     uint8_t a;
 } rgba;
+
+-(NSImage *) boxBlurToImage:(NSImage *)image withRadius:(int)radius {
+    NSBitmapImageRep *bitmapImageRep = [self createBitmapImageRepFromImage:image];
+    const NSInteger width = bitmapImageRep.pixelsWide;
+    const NSInteger height = bitmapImageRep.pixelsHigh;
+    unsigned char *rawImageData = [bitmapImageRep bitmapData];
+    vector<vector<rgba>> pixels(height, vector<rgba>(width));
+    
+    for (long y = 0; y < height; y++) {
+        for (long x = 0; x < width; x++) {
+            long pixelIndex = y * width + x;
+            pixels[y][x].r = rawImageData[pixelIndex * 4];
+            pixels[y][x].g = rawImageData[pixelIndex * 4 + 1];
+            pixels[y][x].b = rawImageData[pixelIndex  * 4 + 2];
+            pixels[y][x].a = rawImageData[pixelIndex * 4 + 3];
+            
+            int sumRed = 0, sumGreen = 0, sumBlue = 0;
+            int count = 0;
+            
+            for (long i = -radius; i <= radius; i++) {
+                for (long j = -radius; j <=radius; j++) {
+                    long pixelX = x + j;
+                    long pixelY = y + i;
+                    
+                    if (pixelX >=  0 && pixelX < width && pixelY >=  0 && pixelY < height) {
+                        rgba pixel = pixels[pixelY][pixelX];
+                        sumRed += pixel.r;
+                        sumGreen += pixel.g;
+                        sumBlue += pixel.b;
+                        count++;
+                    }
+                }
+            }
+            rgba avgPixel;
+            avgPixel.r = roundf(sumRed / count);
+            avgPixel.g = roundf(sumGreen / count);
+            avgPixel.b = roundf(sumBlue / count);
+            
+            rawImageData[pixelIndex *  4] = avgPixel.r;
+            rawImageData[pixelIndex *  4 +  1] = avgPixel.g;
+            rawImageData[pixelIndex *  4 +  2] = avgPixel.b;
+            rawImageData[pixelIndex * 4 + 3] = pixels[y][x].a;
+        }
+    }
+    
+    NSImage *bluredImage = [[NSImage alloc] initWithSize:[bitmapImageRep size]];
+    [bluredImage addRepresentation:bitmapImageRep];
+    
+    return bluredImage;
+}
 
 - (NSImage *) loadImageFromFilePath:(NSString *)filePath {
     return [[NSImage  alloc] initWithContentsOfFile:filePath];
@@ -44,6 +95,7 @@ typedef struct rgba {
 - (NSImage *) imageToGrayScale:(NSImage *)image {
     NSBitmapImageRep *bitmapImageRep = [self createBitmapImageRepFromImage:image];
     unsigned char *rawImageData = [bitmapImageRep bitmapData];
+    
 
     NSInteger allPixelCount = bitmapImageRep.pixelsHigh * bitmapImageRep.pixelsWide;
     for (NSInteger i = 0; i < allPixelCount; ++i) {
@@ -91,11 +143,12 @@ typedef struct rgba {
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     @autoreleasepool {
-        NSString *filePath = @"/Users/motionvfx/Documents/testImg.jpeg";
+        NSString *filePath = @"/Users/motionvfx/Documents/testImg.tiff";
         
         imageView = [[NSImageView alloc] initWithFrame:[self.window.contentView bounds]];
         [imageView setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
         inputImage = [self loadImageFromFilePath:filePath];
+        [[self.window contentView] addSubview:imageView];
     }
 }
 
@@ -126,6 +179,10 @@ typedef struct rgba {
         outputImage = [self imageToGrayScale:inputImage];
     }
     
+    else if ([buttonTitle isEqual:@"Blur"]) {
+        outputImage = [self boxBlurToImage:inputImage withRadius:10];
+    }
+    
     else {
         NSLog(@"Invalid action");
     }
@@ -134,7 +191,6 @@ typedef struct rgba {
     CFTimeInterval elapsedTime = endTime - startTime;
     
     [imageView setImage:outputImage];
-    [[self.window contentView] addSubview:imageView];
     NSLog(@"Time for negative: %.5f seconds", elapsedTime);
 }
     
